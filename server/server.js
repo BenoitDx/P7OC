fc66bb6d0eb8d58a5b116cf42e085dd54706c6a4
BenoitDx <benoit.berche@gmail.Com>
2023-07-28 11:34:55 +0200

    mise en place docker

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = 3001
+
+// Autorise le cross-origin resource sharing
+app.use(cors())
+// Active la conversion du corps d'une requête en JSON
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
