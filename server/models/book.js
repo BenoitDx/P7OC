0978382fd6df6e0ea2ec76df6171d8cec97668a1
BenoitDx <benoit.berche@gmail.Com>
2023-08-05 21:31:18 +0200

    ;;

--- server/models/book.js
+++ server/models/book.js
@@ -6,7 +6,7 @@ const bookSchema = mongoose.Schema({
   author: { type: String, required: true },
   year: { type: Number, required: true },
   genre: { type: String, required: true },
-  ratings : [
+  ratings : [ 
       {
       userId : { type: String, required: true },
       grade : { type: Number, required: true }
